536b7e508238146516c0bb75590a3d880adcba75
RPG141 <157440419+RPG141@users.noreply.github.com>
2025-03-05 19:54:32 -0600

    v1.1
    se agregaron nuevas carpetas (config, presentacion, etc) se agreagon nuevas librerias

---
+++ lib/Dominios/entidades/video_post.dart
@@ -0,0 +1,17 @@
+
+
+class VideoPost {
+  final String caption;
+  final String videoUr1;
+  final int likes;
+  final int views;
+
+  VideoPost({
+    required this.caption,
+    required this.videoUr1,
+    required this.likes,
+    required this.views,
+  });
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto/Temas/app_temas.dart';
+import 'package:proyecto/presetacion/pantallas/discover_screen.dart';
 
 void main() => runApp(const MyApp());
+
+//iniciando la clase main
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,20 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
+      //tema para la app tiktok
       title: 'Tiktok App',
       debugShowCheckedModeBanner: false,
       theme: AppTemas().getTheme(),
 
-      home: Scaffold(
-        
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-
-      ),
+      //llamamos a la clase discover
+      home: DiscoverScreen(),
     );
   }
 }
